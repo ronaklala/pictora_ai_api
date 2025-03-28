70a10a38c9689844cfb07b94c9a6432f60495fb4
ronaklala <54984238+ronaklala@users.noreply.github.com>
2025-03-28 21:33:54 +0530

    Update index.py

--- index.py
+++ index.py
@@ -358,4 +358,4 @@ def search_face():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
